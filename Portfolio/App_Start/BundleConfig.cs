8ed64bbb5edfcffac6fe564118e4211d504dbe92
Alex McFarlane <alex.mcfarlane137@gmail.com>
2018-06-23 23:51:08 -0500

    hero banner

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace Portfolio
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
                       "~/Content/main.css"));
         }
     }
