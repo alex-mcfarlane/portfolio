43e6364ab946d9d41ea4e1e729d5eb73d3776bae
Alex Mcfarlane <amcfarlane1251@gmail.com>
2018-08-10 15:46:58 -0400

    minor UI adjustments

--- Portfolio/Scripts/site.js
+++ Portfolio/Scripts/site.js
@@ -1,1 +1,10 @@
 ﻿var filter = new Filter("filter", "project");
+
+var header = $('header');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 1) {
+        header.addClass('inverted');
+    } else {
+        header.removeClass('inverted');
+    }
+})
